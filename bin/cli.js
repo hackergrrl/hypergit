3194832a5b57dfe2230c917307baec3038e32d74
noffle <sww@eight45.net>
2018-06-04 20:53:54 -0700

    create git remote

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,7 @@ var fs = require('fs')
 var path = require('path')
 var args = require('minimist')(process.argv)
 var hyperdb = require('hyperdb')
+var spawn = require('child_process').spawnSync
 
 if (args._.length === 2) {
   printUsage()
@@ -16,10 +17,17 @@ switch (args._[2]) {
       console.log('A hypergit repo already exists in this directory.')
       return process.exit(1)
     }
+    if (!fs.existsSync(path.join('.git', 'config'))) {
+      console.log('There is no git repository here.')
+      return process.exit(1)
+    }
 
     var db = hyperdb('.hypergit')
     db.ready(function () {
-      console.log('hypergit://' + db.key.toString('hex'))
+      var name = 'swarm'
+      var key = db.key.toString('hex')
+      spawn('git', ['remote', 'add', name, 'hypergit://' + key])
+      console.log('hypergit://' + key)
     })
     break
   default:
