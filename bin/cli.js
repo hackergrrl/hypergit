894de1ef0e80f52eeb14be178f66f8740f3767e2
noffle <sww@eight45.net>
2018-06-07 13:19:09 -0700

    feat(BREAKING): global repo, lots of goodies

--- bin/cli.js
+++ bin/cli.js
@@ -7,6 +7,10 @@ var hyperdb = require('hyperdb')
 var spawn = require('child_process').spawnSync
 var discovery = require('discovery-swarm')
 var swarmDefaults = require('dat-swarm-defaults')
+var envpaths = require('env-paths')('hypergit')
+var mkdirp = require('mkdirp')
+var crypto = require('hypercore/lib/crypto')
+var gitconfig = require('gitconfiglocal')
 
 if (args._.length === 2) {
   printUsage()
@@ -15,10 +19,7 @@ if (args._.length === 2) {
 
 switch (args._[2]) {
   case 'create':
-    ensureNoHypergit()
-
-    var db = getHyperdb()
-    db.ready(function () {
+    getHyperdb(null, function (err, db) {
       var name = 'swarm'
       var key = db.key.toString('hex')
       spawn('git', ['remote', 'add', name, 'hypergit://' + key])
@@ -26,8 +27,6 @@ switch (args._[2]) {
     })
     break
   case 'auth':
-    ensureValidHypergit()
-
     var key = process.argv[3]
     if (!key) {
       console.log('Must include a KEY to authorize.')
@@ -35,8 +34,8 @@ switch (args._[2]) {
     }
     if (typeof key === 'string') key = Buffer.from(key, 'hex')
 
-    var db = getHyperdb()
-    db.ready(function () {
+    getCurrentHyperdb(function (err, db) {
+      if (err) throw err
       db.authorize(key, function (err) {
         if (err) console.log('Failed to authorize:', err)
         else console.log('Authorized write permissions for', key.toString('hex'))
@@ -44,20 +43,45 @@ switch (args._[2]) {
     })
     break
   case 'seed':
-    ensureValidHypergit()
-
-    var db = getHyperdb()
-    db.ready(function () {
-      console.log('Seeding', db.key.toString('hex'))
-      swarmReplicate(db)
+    // seed ALL repos
+    fs.readdir(envpaths.config, function (err, keys) {
+      if (err) throw err
+      var swarm = discovery(swarmDefaults())
+      swarm.listen(2342)
+      keys.forEach(function (key) {
+        getHyperdb(key, function (err, db) {
+          console.log('Seeding', db.key.toString('hex'))
+          swarmReplicate(swarm, db)
+        })
+      })
     })
     break
   case 'id':
-    ensureValidHypergit()
-
-    var db = getHyperdb()
-    db.ready(function () {
-      console.log(db.local.key.toString('hex'))
+    getHypergitRemotes(function (err, remotes) {
+      if (!remotes.length) {
+        console.log('No hypergit remotes on this git repo.')
+        return process.exit(1)
+      } else if (remotes.length === 1) {
+        var key = remotes[0].url.replace('hypergit://', '')
+        getHyperdb(key, function (err, db) {
+          if (err) throw err
+          console.log(db.local.key.toString('hex'))
+        })
+      } else if (!process.argv[3]) {
+        console.log('Multiple hypergit remotes present. Specify which one you\'d like the id of.')
+        return process.exit(1)
+      } else {
+        var remote = config.remote[process.argv[3]]
+        if (!remote) {
+          console.log('No hypergit remote by that name.')
+          return process.exit(1)
+        }
+        var key = remote.url.replace('hypergit://', '')
+        getHyperdb(key, function (err, db) {
+          if (err) throw err
+          console.log(db.local.key.toString('hex'))
+        })
+      }
     })
     break
   default:
@@ -65,12 +89,52 @@ switch (args._[2]) {
     break
 }
 
-function getHyperdb () {
-  return hyperdb('.hypergit')
+function hyperdir (key) {
+  return path.join(envpaths.config, key)
 }
 
-function ensureNoHypergit () {
-  if (fs.existsSync('.hypergit')) {
+function getCurrentHyperdb (cb) {
+  gitconfig('.', function (err, config) {
+    if (err) throw err
+    var remotes = Object.keys(config.remote)
+      .map(function (key) {
+        var remote = config.remote[key]
+        remote.name = key
+        return remote
+      })
+      .filter(function (remote) {
+        return remote.url.startsWith('hypergit://')
+      })
+
+    if (!remotes.length) {
+      cb(new Error('no hypergit remotes here'))
+    } else if (remotes.length === 1) {
+      var key = remotes[0].url.replace('hypergit://', '')
+      getHyperdb(key, cb)
+    } else {
+      cb(new Error('multiple hypergit remotes here'))
+    }
+  })
+}
+
+function getHyperdb (key, cb) {
+  var db
+  if (key === null) {
+    // create
+    var keypair = crypto.keyPair()
+    key = keypair.publicKey.toString('hex')
+    db = hyperdb(path.join(envpaths.config, key), key, {secretKey: keypair.secretKey})
+  } else {
+    // existing
+    db = hyperdb(path.join(envpaths.config, key), key)
+  }
+  db.ready(function () {
+    cb(null, db)
+  })
+}
+
+function ensureNoHypergit (key) {
+  if (fs.existsSync(hyperdir(key))) {
     console.log('A hypergit repo already exists in this directory.')
     return process.exit(1)
   }
@@ -81,7 +145,7 @@ function ensureNoHypergit () {
 }
 
 function ensureValidHypergit () {
-  if (!fs.existsSync('.hypergit')) {
+  if (!fs.existsSync(hyperdir(key))) {
     console.log('No hypergit repo exists in this directory.')
     return process.exit(1)
   }
@@ -91,23 +155,35 @@ function ensureValidHypergit () {
   }
 }
 
+function getHypergitRemotes (cb) {
+  gitconfig('.', function (err, config) {
+    if (err) return cb(err)
+    var remotes = Object.keys(config.remote)
+      .map(function (key) {
+        var remote = config.remote[key]
+        remote.name = key
+        return remote
+      })
+      .filter(function (remote) {
+        return remote.url.startsWith('hypergit://')
+      })
+    cb(null, remotes)
+  })
+}
+
 function printUsage () {
   require('fs')
     .createReadStream(path.join(__dirname, '/usage.txt'))
     .pipe(process.stdout)
 }
 
-function swarmReplicate (db) {
+function swarmReplicate (swarm, db) {
   var key = db.key.toString('hex')
-  console.log('id', db.local.key.toString('hex'))
-  var swarm = discovery(swarmDefaults({
-    id: db.local.key,
-  }))
-  swarm.listen(2342)
+  //console.log('id', db.local.key.toString('hex'))
   swarm.join(key)
   swarm.on('connection', function (conn, info) {
     console.log('found peer', info.id.toString('hex'))
-    var r = db.replicate() 
+    var r = db.replicate({live:true})
     r.pipe(conn).pipe(r) 
     r.once('end', function () {
       console.error('done replicating', info.id.toString('hex'))
