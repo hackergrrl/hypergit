b20d8971d1e668e2fb213e7fc86af9a72ebffaf2
noffle <sww@eight45.net>
2018-06-07 10:41:21 -0700

    feat: "seed" and "id" subcommands

--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,8 @@ var path = require('path')
 var args = require('minimist')(process.argv)
 var hyperdb = require('hyperdb')
 var spawn = require('child_process').spawnSync
+var discovery = require('discovery-swarm')
+var swarmDefaults = require('dat-swarm-defaults')
 
 if (args._.length === 2) {
   printUsage()
@@ -38,6 +40,23 @@ switch (args._[2]) {
         if (err) console.log('Failed to authorize:', err)
         else console.log('Authorized write permissions for', key)
       })
+    })
+    break
+  case 'seed':
+    ensureValidHypergit()
+
+    var db = getHyperdb()
+    db.ready(function () {
+      console.log('Seeding', db.key.toString('hex'))
+      swarmReplicate(db)
+    })
+    break
+  case 'id':
+    ensureValidHypergit()
+
+    var db = getHyperdb()
+    db.ready(function () {
+      console.log(db.local.key.toString('hex'))
     })
     break
   default:
@@ -75,4 +94,20 @@ function printUsage () {
   require('fs')
     .createReadStream(path.join(__dirname, '/usage.txt'))
     .pipe(process.stdout)
+}
+
+function swarmReplicate (db) {
+  var key = db.key.toString('hex')
+  var swarm = discovery(swarmDefaults({
+    id: db.local.key,
+  }))
+  swarm.join(key)
+  swarm.on('connection', function (conn, info) {
+    console.log('found peer', info)
+    var r = db.replicate() 
+    r.pipe(conn).pipe(r) 
+    r.once('end', function () {
+      console.error('done replicating', info)
+    })
+  })
 }
