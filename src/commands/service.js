60f7b90e90412fae115b149f125a80e186cc2c82
William Hilton <wmhilton@gmail.com>
2018-06-29 19:40:02 -0400

    Make sure service install/remove runs as admin

--- src/commands/service.js
+++ src/commands/service.js
@@ -1,5 +1,7 @@
-var service = require('os-service')
 var console2file = require('console2file').default
+var os_service = require('os-service')
+var sudo = require('sudo-prompt')
+var isElevated = require('is-elevated')
 var seed = require('./seed')
 
 function logresult (status) {
@@ -12,13 +14,40 @@ function logresult (status) {
   }
 }
 
+function logoutput (err, stdout, stderr) {
+  if (err) {
+    console.log(err)
+  } else {
+    if (stderr) {
+      console.log(stderr)
+    }
+    console.log(stdout)
+  }
+}
+
+function formatArgvString (argv) {
+  return argv.map(x => `"${x}"`).join(' ')
+}
+
 module.exports = function service (command) {
   switch (command) {
     case 'install':
-      service.add("hypergit", { programArgs: ["service", "seed"] }, logresult('installed'))
+      isElevated().then(function (elevated) {
+        if (elevated) {
+          os_service.add("hypergit", { programArgs: ["service", "seed"] }, logresult('installed'))
+        } else {
+          sudo.exec(formatArgvString(process.argv), {name: 'hypergit CLI'}, logoutput)
+        }
+      })
       break
     case 'remove':
-      service.remove("hypergit", logresult('removed'))
+      isElevated().then(function (elevated) {
+        if (elevated) {
+          os_service.remove("hypergit", logresult('removed'))
+        } else {
+          sudo.exec(formatArgvString(process.argv), {name: 'hypergit CLI'}, logoutput)
+        }
+      })
       break
     case 'seed':
       fs.unlink(path.join(envpaths.config, 'log.txt'), () => {
@@ -26,7 +55,7 @@ module.exports = function service (command) {
           filePath: path.join(envpaths.config, 'log.txt'),
           fileOnly: false
         })
-        service.run(() => service.stop())
+        os_service.run(() => os_service.stop())
         // seed ALL repos
         seed()
       })
